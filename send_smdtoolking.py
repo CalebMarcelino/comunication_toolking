a01bddb0d5cb894736116d791bed1c7ad0ca3913
CalebMarcelino <119153272+CalebMarcelino@users.noreply.github.com>
2023-04-25 22:06:07 +0100

    Update send_smdtoolking.py

--- send_smdtoolking.py
+++ send_smdtoolking.py
@@ -1,1 +1,26 @@
+import socket
 
+# Define the host and port for the server
+HOST = input('ip: ')  # localhost
+PORT = 12345
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+s.connect((HOST, PORT))
+
+print('Este socket foi criado pelo: Mr Code')
+while True:
+    # Send a message to the server
+    message = input('Digite a mensagem  (ou digite q para sair): ')
+    if message == 'q':
+        break
+    s.sendall(message.encode())
+
+    # Receive and print the echoed message from the server
+    data = s.recv(1024)
+    print('Received:', data.decode())
+
+# Close the connection
+s.close()
